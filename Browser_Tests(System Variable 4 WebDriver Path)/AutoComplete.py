e8e5879bde6b6cfe4f03f9bdda0759a42bf3157a
Juan Cordoba <juancocr@gmail.com>
2020-03-05 13:34:38 -0600

    New Changes

--- Browser_Tests(System Variable 4 WebDriver Path)/AutoComplete.py
+++ Browser_Tests(System Variable 4 WebDriver Path)/AutoComplete.py
@@ -2,25 +2,15 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
-class Autocomplete():
+baseURL = 'https://www.redbus.in/'
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.implicitly_wait(10)
+driver.get(baseURL)
 
-    def test2(self):
-        baseUrl = "http://www.southwest.com"
-        driver = webdriver.Firefox()
-        driver.maximize_window()
-        driver.get(baseUrl)
-        driver.implicitly_wait(3)
+startlocation = driver.find_element(By.ID, "src")
+startlocation.send_keys('Pun')
 
-        # Send Partial Data
-        cityField = driver.find_element_by_id("LandingAirBookingSearchForm_originationAirportCode")
-        cityField.send_keys("New York")
-        time.sleep(3)
-        # Find the item and click
-        itemToSelect = driver.find_element_by_xpath("//input[contains(text(),'LGA')]")
-        itemToSelect.click()
-
-        # time.sleep(3)
-        # driver.quit()
-
-ff = Autocomplete()
-ff.test2()
+time.sleep(2)
+selectloc = driver.find_element(By.XPATH, "//ul[@class='autoFill']//li[contains(text(),'Wagholi')]")
+selectloc.click()
